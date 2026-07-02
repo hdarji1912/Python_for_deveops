adeb501f69a8c2bb1ec2684f633816ec01c130d2
hdarji1912 <hdarji783@gmail.com>
2026-07-02 07:39:11 +0530

    feat: add Python script for CPU and disk usage checks

---
+++ utils.py
@@ -0,0 +1,10 @@
+import os  # importing python library into code
+
+
+print(os.system('date /t'))  # command to get date 
+print(os.system('df -h'))   # linux commands to get system information
+print(os.system('systeminfo')) # windows commands to get system information
+print(os.system('ipconfig')) 
+print(os.system('dir')) 
+
+# windows commands will run in vs code to check cpu and disk performance.
